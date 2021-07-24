8c90cc9e0d2dcab3d5417e1a7686b94ac4da1f17
Geeta Upadhyay <80974845+geetaupadhyay@users.noreply.github.com>
2021-07-25 00:53:23 +0530

    Delete Duplicate node

---
+++ Data Structures/Linked Lists/Delete duplicate-value nodes from a sorted linked list Hackerrank.cpp
@@ -0,0 +1,30 @@
+/*
+ * Complete the 'removeDuplicates' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ * For your reference:
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };*/
+
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* llist) {
+    if (!llist){
+        return llist;
+    }
+    SinglyLinkedListNode* Curr= llist;
+    if (Curr->next==NULL){
+        return llist;
+    }
+    while(Curr!=NULL && Curr->next!=NULL){
+        while(Curr->next!=NULL && Curr->data==Curr->next->data){
+            Curr->next=Curr->next->next;
+        }
+        if (Curr->next!=NULL){
+            Curr=Curr->next;
+        }
+    }
+    return llist;
+}
+
